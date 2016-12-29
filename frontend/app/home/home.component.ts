914a6498ab15cd06836364df63b8c463d344a9bc
Julmer Olivero <jolivero.03gmail.com>
2016-12-29 19:07:51 -0400

    Puesta a punto de la Base de los Posts

--- frontend/app/home/home.component.ts
+++ frontend/app/home/home.component.ts
@@ -26,20 +26,7 @@ export class HomeComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.loadAllUsers();
-        // reset login status
 
-        //this.authenticationService.logout();
-
-        // get return url from route parameters or default to '/'
-        //this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
     }
 
-    deleteUser(id: number) {
-        this.userService.delete(id).subscribe(() => { this.loadAllUsers() });
-    }
-
-    private loadAllUsers() {
-        this.userService.getAll().subscribe(users => { this.users = users; });
-    }
 }
