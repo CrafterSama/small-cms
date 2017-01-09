96fd8af12092fee05f8af7fb25a45d3e738f1641
Julmer Olivero Ocando <Julmer Olivero Ocando>
2017-01-08 20:26:07 -0400

    Testeando el Mapa de Contactenos

--- frontend/app/contact/contact.component.ts
+++ frontend/app/contact/contact.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit, NgModule } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
-import { AgmCoreModule } from 'angular2-google-maps/core';
+//import { AgmCoreModule } from 'angular2-google-maps/core';
 
 import { AlertService, UserService } from '../_services/index';
-
 
 @Component({
     moduleId: module.id,
@@ -12,15 +11,22 @@ import { AlertService, UserService } from '../_services/index';
 })
 
 
-export class ContactComponent implements OnInit {
+export class ContactComponent {
+    loading = false;
+	lat: number = 10.200905;
+	lng: number = -71.314232; 
 
 	constructor (private alertService: AlertService) {}
 
 	title: string = 'Datos de Contacto';
-	content: string = '<div class="contact-data"><strong>Direccion:</strong><address>Calle Venecia, Oficinas Softars, Ciudad Ojeda, Edo. Zulia, Venezuela</address><br /><strong>Telefono: </strong><span>+58 265 000 00 00</span></div>';
+	content: string = '<div class="contact-data"><strong>Direccion:</strong><address>Calle Venecia, Oficinas Softars, Ciudad Ojeda, Edo. Zulia, Venezuela.</address><strong>Telefono: </strong><span>+58 265 000 00 00</span></div>';
 
 	ngOnInit() {
 
+    }
+
+    contactMessage() {
+    	loading = true;
     }
 
 }
